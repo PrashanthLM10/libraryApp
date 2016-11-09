5484b9e6bc187e64899704b279068942f45f4137
PrashanthLM10 <prashanth.sunny0236@gmail.com>
2016-11-09 20:01:54 +0530

    collections page changes

--- app/modules/collections/js/controllers.js
+++ app/modules/collections/js/controllers.js
@@ -1,8 +1,8 @@
-angular.module('library.collections.controllers',['library.collections.services']).controller('collectionsController',function($scope){
+angular.module('library.collections.controllers',['library.collections.services','ngAnimate']).controller('collectionsController',function($scope){
         
 }).controller('categoriesController',function($scope,categoriesService){
         categoriesService.getCollections().success(function(data,status,config,headers){
-                console.log("getCollections:Success",data);                
+                    $scope.categories = data;
             }).error(function(data,status,config,headers){
                 console.log("getCollections:Error");
             });
--- app/modules/collections/js/services.js
+++ app/modules/collections/js/services.js
@@ -1,6 +1,5 @@
 angular.module('library.collections.services',[]).service('categoriesService',function($http){
         this.getCollections = function(){
-            console.log("in collections");
             return $http.get('/collections');
         };
     });
